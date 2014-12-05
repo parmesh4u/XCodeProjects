b58054198e8b11ef4eec60a9708e853c1fcce0ff
Parmeshwar <mannu.ims@gmail.com>
2014-12-05 22:49:08 +0530

    Adding initial Xcode iOS project

---
+++ GitHubTest/GitHubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by Parmeshwar on 12/5/14.
+//  Copyright (c) 2014 Parmeshwar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubTest/GitHubTest/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  GitHubTest
+//
+//  Created by Parmeshwar on 12/5/14.
+//  Copyright (c) 2014 Parmeshwar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
